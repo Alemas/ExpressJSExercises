873a419a9788e634e878b0485edf671aa7767991
Alemas <mateusreckziegel@gmail.com>
2017-07-10 12:00:32 -0300

    Minor changes

--- helloworld/app.js
+++ helloworld/app.js
@@ -2,7 +2,20 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-  response.send('Hello World');
+
+  //Uses Express API
+  //response.send('Hello World');
+
+  //Uses Node API
+  response.write('Hello World');
+  reponse.end();
+});
+
+app.get('/blocks', function(request, response) {
+  var blocks = ['Fixed', 'Movable', 'Rotating'];
+
+  //response converts automatically the array to json
+  response.send(blocks);
 });
 
 app.listen(3000, function() {
