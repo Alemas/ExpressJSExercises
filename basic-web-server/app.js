e6a2407134e2d7087b38dc5da54f291367022b49
Alemas <mateusreckziegel@gmail.com>
2017-07-11 13:30:12 -0300

    Added partial templates support to basic-web-server

--- basic-web-server/app.js
+++ basic-web-server/app.js
@@ -5,6 +5,9 @@ const hbs = require('hbs');
 
 var app = express();
 
+//Register the path to partials templates for hbs
+hbs.registerPartials(__dirname + '/views/partials');
+
 //Assigns 'hbs' to value 'view engine' on the app settings table
 app.set('view engine', 'hbs');
 
@@ -15,9 +18,20 @@ app.use(express.static(__dirname + '/public'));
 app.get('/about', (request, response) => {
 
   //'views' is the default directory that ExpressJS uses for templates
-  //'render' loads .hbs files from 'views folder'
-  response.render('about.hbs');
+  //'render' loads .hbs files from 'views folder' passing arguments through a json (optional)
+  response.render('about.hbs', {
+    pageTitle : 'About',
+    currentYear : new Date().getFullYear()
+  });
 });
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle : 'Home',
+    welcomeMessage : 'Welcome to my this',
+    currentYear : new Date().getFullYear()
+  });
+})
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
